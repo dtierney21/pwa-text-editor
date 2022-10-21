1df7b5631e0ecc9064be75ab55de76a7c8c79a84
Dustin Tierney <dustintierney@rockethomes.com>
2022-10-21 13:36:34 -0400

    added webpack config logic

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,24 +7,63 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+    return {
+        mode: 'development',
+        entry: {
+            main: './src/js/index.js',
+            install: './src/js/install.js',
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            // Webpack plugin that generates our html file and injects our bundles.
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                title: 'Contact Cards',
+            }),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+            // Creates a manifest.json file.
+            new WebpackPwaManifest({
+                fingerprints: false,
+                inject: true,
+                name: 'Contact Cards',
+                short_name: 'Contact',
+                description: 'Never forget your contacts!',
+                background_color: '#225ca3',
+                theme_color: '#225ca3',
+                start_url: './',
+                publicPath: './',
+                icons: [
+                    {
+                        src: path.resolve('src/images/logo.png'),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join('assets', 'icons'),
+                    },
+                ],
+            }),
+        ],
+
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    // We use babel-loader in order to use ES6.
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                        },
+                    },
+                },
+            ],
+        },
+    };
 };
