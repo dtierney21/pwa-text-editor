cc3ce105d7b8df4fb249932b9085ac638cc0f6e6
Dustin Tierney <dustintierney@rockethomes.com>
2022-10-21 14:50:52 -0400

    fixed icon path and ran build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
     return {
@@ -39,9 +38,17 @@ module.exports = () => {
                 theme_color: '#7eb4e2',
                 start_url: './',
                 publicPath: './',
+                icons: [
+                    {
+                        src: path.resolve('src/images/logo.png'),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join('assets', 'icons'),
+                    },
+                ],
             }),
         ],
 
+        // TODO: Add CSS loaders and babel to webpack.
         module: {
             rules: [
                 {
