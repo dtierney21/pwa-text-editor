0064976c2edb660c5ec979a874472f9d7bc62f1d
Dustin Tierney <dustintierney@rockethomes.com>
2022-10-21 13:44:27 -0400

    updated webpack config with project valaues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,27 +21,24 @@ module.exports = () => {
             // Webpack plugin that generates our html file and injects our bundles.
             new HtmlWebpackPlugin({
                 template: './index.html',
-                title: 'Contact Cards',
+                title: 'JATE',
+            }),
+
+            // Injects our custom service worker
+            new InjectManifest({
+                swSrc: './src-sw.js',
+                swDest: 'src-sw.js',
             }),
 
             // Creates a manifest.json file.
             new WebpackPwaManifest({
-                fingerprints: false,
-                inject: true,
-                name: 'Contact Cards',
-                short_name: 'Contact',
-                description: 'Never forget your contacts!',
-                background_color: '#225ca3',
-                theme_color: '#225ca3',
+                name: 'JATE',
+                short_name: 'JATE',
+                description: 'Text Editor',
+                background_color: '#7eb4e2',
+                theme_color: '#7eb4e2',
                 start_url: './',
                 publicPath: './',
-                icons: [
-                    {
-                        src: path.resolve('src/images/logo.png'),
-                        sizes: [96, 128, 192, 256, 384, 512],
-                        destination: path.join('assets', 'icons'),
-                    },
-                ],
             }),
         ],
 
